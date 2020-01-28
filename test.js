f13786df1f7b1f90151415c7ae6224c7d5ccdd50
Ivan Demidov <Scrum@list.ru>
2020-01-28 11:22:27 +0300

    test: revrite test on run in server

--- test.js
+++ test.js
@@ -1,12 +1,20 @@
+const http = require('http');
 const test = require('ava');
 const {JSDOM} = require('jsdom');
+const listen = require('test-listen');
+const app = require('./app');
 
-test('first iframe test', t => {
-	t.plan(1);
-
-	return JSDOM.fromFile('./index.html', {}).then(dom => {
-		const iframe = dom.window.document.querySelector('iframe');
-		t.true(iframe instanceof dom.window.HTMLIFrameElement);
-	});
+test.before(async t => {
+	t.context.server = http.createServer(app);
+	t.context.baseUrl = await listen(t.context.server);
 });
 
+test.after.always(t => {
+	t.context.server.close();
+});
+
+test('title', async t => {
+	const {window} = await JSDOM.fromURL(t.context.baseUrl, {});
+	const iframe = window.document.querySelector('iframe');
+	t.true(iframe instanceof window.HTMLIFrameElement);
+});
